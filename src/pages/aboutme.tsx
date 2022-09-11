4dec39257115ee093e0d7ca25c9ba1856109b475
arunkulkarni95 <arunkulkarni95@hotmail.com>
2022-09-11 18:38:54 -0400

    fix styling

--- src/pages/aboutme.tsx
+++ src/pages/aboutme.tsx
@@ -5,22 +5,22 @@ import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 
 export const AboutMe = () => 
-  <div className={styles.container}>
-    <Head>
-      <title>About Me | Arun Kulkarni</title>
-      <meta name="description" content="About Arun Kulkarni" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+  <>
     <NavBar/>
-    <main className={styles.main}>
-      <h1 className={styles.title}>About Me</h1>
-
-      <div className={styles.description}>
+    <div className={styles.container}>
+      <Head>
+        <title>About Me | Arun Kulkarni</title>
+        <meta name="description" content="About Arun Kulkarni" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>About Me</h1>
+        <div className={styles.description}>
           Coming soon!
-      </div>
-    </main>
-    <Footer />
-  </div>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  </>
   
-
 export default AboutMe;
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -5,22 +5,22 @@ import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 
 export const Contact = () => 
-  <div className={styles.container}>
-    <Head>
-      <title>About Me | Arun Kulkarni</title>
-      <meta name="description" content="About Arun Kulkarni" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+  <>
     <NavBar/>
-    <main className={styles.main}>
-      <h1 className={styles.title}>Contact Me</h1>
-
-      <div className={styles.description}>
+    <div className={styles.container}>
+      <Head>
+        <title>About Me | Arun Kulkarni</title>
+        <meta name="description" content="About Arun Kulkarni" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Contact Me</h1>
+        <div className={styles.description}>
           Coming soon!
-      </div>
-    </main>
-    <Footer />
-  </div>
-  
+        </div>
+      </main>
+      <Footer />
+    </div>
+  </>
 
 export default Contact;
